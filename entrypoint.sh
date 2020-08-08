bae96d73cef1d6edb87c3020e27252cf052bf0ad
Meng Li <Mengli619@github.com>
2020-08-08 18:08:56 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ RTMP_SERVER=${RTMP_SERVER:-rtmp://host.docker.internal/live}
 
 for i in $(seq 1 $SOURCE_COUNT); do
   echo "publish source$i"
-  ffmpeg -re -stream_loop -1 -i sources/source$i.mp4 -c copy -f flv ${RTMP_SERVER}/source$i </dev/null > /dev/null 2>&1 &
+  ffmpeg -re -stream_loop -1 -i sources/source$i -c copy -f flv ${RTMP_SERVER}/source$i </dev/null > /dev/null 2>&1 &
 done
 
 echo "wait for streaming completed..."
