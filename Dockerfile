fe540b9c3f8aa72485967736b0029aa05effd97b
Meng Li <Mengli619@github.com>
2020-08-13 11:04:52 +0800

    use testsrc

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM jrottenberg/ffmpeg:4.1-ubuntu
 
 RUN apt-get install -y wget
 
-COPY sources.txt download.sh entrypoint.sh ./
-RUN sh download.sh
+COPY SourceCodePro-Regular.ttf entrypoint.sh ./
 
 ENTRYPOINT ["sh", "entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,18 @@ set -e
 
 SOURCE_COUNT=${SOURCE_COUNT:-8}
 RTMP_SERVER=${RTMP_SERVER:-rtmp://host.docker.internal/live}
+RESOLUTION=${RESOLUTION-640x360}
+FRAME_RATE=${FRAME_RATE-25}
 
 for i in $(seq 1 $SOURCE_COUNT); do
   echo "publish source$i"
-  ffmpeg -re -stream_loop -1 -i sources/source$i -c copy -f flv ${RTMP_SERVER}/source$i </dev/null > /dev/null 2>&1 &
+  ffmpeg \
+    -re \
+    -f lavfi \
+    -i testsrc=size=${RESOLUTION}:rate=${FRAME_RATE} \
+    -vf drawtext="SourceCodePro-Regular.ttf:text=source$i:x=(w-tw)/2:y=(2*lh):fontsize=40" \
+    -f flv \
+    ${RTMP_SERVER}/source$i &
 done
 
 echo "wait for streaming completed..."
