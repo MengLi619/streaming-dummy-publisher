7061614366000c8964c783a79a496f9e22c7e2db
Meng Li <Mengli619@github.com>
2020-08-31 15:06:59 +0800

    generate sine audio

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,17 @@ SOURCE_COUNT=${SOURCE_COUNT:-8}
 RTMP_SERVER=${RTMP_SERVER:-rtmp://host.docker.internal/live}
 RESOLUTION=${RESOLUTION:-640x360}
 FRAME_RATE=${FRAME_RATE:-25}
+AUDIO_START_FREQUENCY=${AUDIO_START_FREQUENCY:-400}
+AUDIO_FREQUENCY_INTERVAL=${AUDIO_FREQUENCY_INTERVAL:-100}
 
 for i in $(seq 1 $SOURCE_COUNT); do
   echo "publish source$i"
   ffmpeg \
     -re \
-    -f lavfi \
-    -i testsrc=size=${RESOLUTION}:rate=${FRAME_RATE} \
+    -f lavfi -i testsrc=size=${RESOLUTION}:rate=${FRAME_RATE} \
+    -f lavfi -i sine=f=$((AUDIO_START_FREQUENCY + i * AUDIO_FREQUENCY_INTERVAL)) \
     -vf drawtext="SourceCodePro-Regular.ttf:text=source$i:x=(w-tw)/2:y=(2*lh):fontsize=40" \
+    -shortest \
     -f flv \
     ${RTMP_SERVER}/source$i &
 done
