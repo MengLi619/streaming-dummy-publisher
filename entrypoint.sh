01858666f2d54ab133419c5f1a5acefb65e35a0d
Meng Li <Mengli619@github.com>
2020-09-16 08:53:08 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,13 +9,19 @@ AUDIO_START_FREQUENCY=${AUDIO_START_FREQUENCY:-400}
 AUDIO_FREQUENCY_INTERVAL=${AUDIO_FREQUENCY_INTERVAL:-100}
 
 for i in $(seq 1 $SOURCE_COUNT); do
-  echo "publish source$i"
+  echo "publishing source$i"
   ffmpeg \
     -re \
+    -r 24 \
     -f lavfi -i testsrc=size=${RESOLUTION}:rate=${FRAME_RATE} \
     -f lavfi -i sine=f=$((AUDIO_START_FREQUENCY + i * AUDIO_FREQUENCY_INTERVAL)) \
     -vf drawtext="SourceCodePro-Regular.ttf:text=source$i:x=(w-tw)/2:y=(2*lh):fontsize=40" \
-    -shortest \
+    -vcodec libx264 \
+    -preset ultrafast \
+    -tune zerolatency \
+    -pix_fmt yuv420p \
+    -x264opts "keyint=25:min-keyint=25:no-scenecut" \
+    -acodec aac \
     -f flv \
     ${RTMP_SERVER}/source$i &
 done
